27f56725bdf4a5e7dd3a108de170112a4f69bf90
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-30 18:04:23 -0300

    fix: :bug: Tornando variáveis de ambiente válidas

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
-const PORT = process.env.PORT || 3000;
+const hereIsTheUpsideDown = Boolean(process.env.UPSIDEDOWN_MODE === 'true');
+const PORT = Number(process.env.PORT) || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
